ec57ee112d89a4a33c2064ff092de339098b27c0
ZRunner <z.runner.mc@gmail.com>
2019-05-16 19:14:00 +0200

    Ajout des couleurs

--- start.pas
+++ start.pas
@@ -1,6 +1,6 @@
 program Projet;
 
-uses fpjson, jsonparser;
+uses fpjson, jsonparser, Crt;
 
 
 Type carte = Record
@@ -12,6 +12,7 @@ Type joueur=Record
     cartes:array of carte;
     pseudo:string;
     age:integer;
+    couleur:byte;
     end;
 
 Type config = Record
@@ -34,8 +35,8 @@ var d:deck;
 function inarray(liste:array of carte;card:carte):boolean;
 var i:integer;
 begin
-    for i:=0 to length(liste) do
-        if (liste[1].couleur = card.couleur) and (liste[1].valeur = card.valeur) then Exit(True);
+    for i:=0 to high(liste) do
+        if (liste[i].couleur = card.couleur) and (liste[i].valeur = card.valeur) then Exit(True);
     Exit(False);
 end;
 
@@ -53,11 +54,18 @@ begin
             end;
 end;
 
+// Cette fonction pourra facilement être changée lorsqu'on entamera la partie graphique
+procedure display_text(a:string);
+begin
+    writeln(a)
+end;
+
+// Chargement de la configuration du jeu
 function loadconfig:config;
 var jData:TJSONData;
     jObject : TJSONObject;
 begin
-    jData := GetJSON('{"max_players":4,"win_default":10,"win":2,"loose":-2,"min_age":5,"max_age":100}');
+    jData := GetJSON('{"max_players":5,"win_default":10,"win":2,"loose":-2,"min_age":5,"max_age":100}');
     jObject := TJSONObject(jData);
     loadconfig.players := jObject.Get('max_players');
     loadconfig.win_defaut := jObject.Get('win_default');
@@ -68,11 +76,13 @@ begin
 end;
 
 
-
-function creerjoueur:joueur;
+// Création d'un joueur, avec son pseudo, sa couleur et son age
+function creerjoueur(couleur:byte;colorname:string):joueur;
 var age:integer;pseudo:string;ok:boolean;
 begin
-    write('Indiquez votre pseudo',#10,'> ');readln(pseudo);
+    textcolor(couleur);
+    creerjoueur.couleur := couleur;
+    write('Joueur ',colorname,', indiquez votre pseudo',#10,'> ');readln(pseudo);
     {$I-}   {compiler directive, removes abort on IO error}
     write('Indiquez votre âge',#10,'> ');readln(age);
     while (conf.min_age>age) or (age>conf.max_age) or (IOResult <> 0) do begin
@@ -82,38 +92,42 @@ begin
     {$I+}   {restores default IO checking} 
     creerjoueur.age := age;
     creerjoueur.pseudo := pseudo;
+    writeln;
 end;
 
+// Création de la liste de tous les joueurs
 procedure creerjoueurs(var liste:joueurs);
 var i:integer;
+    cls:array[0..4] of byte=(red,yellow,blue,green,,LightMagenta,Brown,White);
+    clsn:array[0..4] of string=('Rouge','Jaune','Bleu','Vert','Violet','Marron','Blanc');
 begin
     for i:=0 to high(liste) do
-        liste[i] := creerjoueur;
+        liste[i] := creerjoueur(cls[i],clsn[i]);
+    normvideo;
 end;
 
 procedure distribuer(var liste:joueurs;n:integer);
-var i,j,k,p:integer;
+var i,k,p:integer;
     utilises:deck;
 begin
     k := 0;
     for p:=0 to high(liste) do begin
         setlength(liste[p].cartes,n);
-        j := 0;
-        for i:=0 to n do begin
-            liste[p].cartes[j] := d[random(52)];
-            while inarray(utilises,liste[p].cartes[j]) do
-                liste[p].cartes[j] := d[random(52)];
-            utilises[k] := liste[p].cartes[j];
-            j := j+1;
+        for i:=0 to n-1 do begin
+            liste[p].cartes[i] := d[random(52)];
+            while inarray(utilises,liste[p].cartes[i]) do
+                liste[p].cartes[i] := d[random(52)];
+            utilises[k] := liste[p].cartes[i];
             k := k+1;
             end;
         end;
 end;
 
 procedure partie(liste:joueurs);
+var c:carte;
 begin
     distribuer(liste,5);
-    writeln(liste[0].cartes[0].couleur,' ',liste[0].cartes[0].valeur);
+    c := liste[0].cartes[0];
 end;
 
 
@@ -124,8 +138,5 @@ begin
     conf := loadconfig;
     setlength(liste,conf.players);
     creerjoueurs(liste);
-    writeln(d[0].couleur,' ',d[0].valeur);
-    writeln(d[32].couleur,' ',d[32].valeur);
-    writeln(d[51].couleur,' ',d[51].valeur);
     partie(liste);
 end.
