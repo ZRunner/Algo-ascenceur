a3dce59edb46dc6d286bd1daeb27f546bfb7dbdc
Z_runner <arthur.blaise.23@gmail.com>
2019-06-03 14:15:36 +0200

    Update classes.pas

--- classes.pas
+++ classes.pas
@@ -6,16 +6,31 @@ interface
 
 uses gLib2D;
 
-Type joueur=Record // L'un des joueurs
-    pseudo:string;
-    couleur:byte;
-end;
-Type joueurs=array of joueur;
+Type config = Record // configuration de la partie
+    players:integer;
+    win_defaut:integer;
+    win:integer;
+    loose:integer;
+    min_age:integer;
+    max_age:integer;
+    default_window_size:integer;
+    end;
 
 Type carte = Record // L'une des 52 cartes du jeu
     couleur:string;
     valeur:integer;
 end;
+
+Type joueur=Record // L'un des joueurs
+    cartes:array of carte;
+    pseudo:string;
+    age:integer;
+    couleur:byte;
+    end;
+Type joueurs=array of joueur;
+
+
+Type deck = array[0..51] of carte; // Liste de toutes les cartes du jeu, triées
 
 type joueur_graph=Record // uniquement les infos utiles
     pseudo:string;
