95a63df6af8909413b448a7807d8bc38f05fa8e0
Z_runner <arthur.blaise.23@gmail.com>
2019-06-03 14:16:23 +0200

    changement des classes

--- start.pas
+++ start.pas
@@ -1,31 +1,7 @@
 program Projet;
 
-uses fpjson, jsonparser, Intro, Crt, sysutils;
+uses fpjson, jsonparser, Intro, Crt, sysutils, classes;
 
-Type carte = Record // L'une des 52 cartes du jeu
-    couleur:string;
-    valeur:integer;
-    end;
-
-Type joueur=Record // L'un des joueurs
-    cartes:array of carte;
-    pseudo:string;
-    age:integer;
-    couleur:byte;
-    end;
-
-Type config = Record // configuration de la partie
-    players:integer;
-    win_defaut:integer;
-    win:integer;
-    loose:integer;
-    min_age:integer;
-    max_age:integer;
-    default_window_size:integer;
-    end;
-
-Type deck = array[0..51] of carte; // Liste de toutes les cartes du jeu, triées
-Type joueurs=array of joueur; // Liste de tous les joueurs du jeu
 
 var d:deck;
     liste:joueurs;
