82f63cea9f522d46f9a24ceb366b3690aa3d7d29
Z_runner <arthur.blaise.23@gmail.com>
2019-06-06 16:43:39 +0200

    ajout détection clic

--- tests/example.pas
+++ tests/example.pas
@@ -4,7 +4,7 @@ uses gLib2D, graph, classes, sysutils, Crt;
 
 
 var joueurs_list:classes.joueursArray;
-    une_carte:classes.carte;
+    une_carte,atout:classes.carte;
     liste_cartes:CartesArray;
     liste_manche:array[0..4] of classes.carte;
     image:classes.background;
@@ -29,8 +29,10 @@ begin
     for i:=0 to high(liste_cartes) do begin
         une_carte.couleur := 'carreau';
         une_carte.valeur := round(random*10)+1;
-        liste_cartes[i] := convert_carte(une_carte);
-        if i<5 then liste_manche[i] := convert_carte(une_carte);
+        liste_cartes[i] := une_carte;
+        convert_carte(liste_cartes[i]);
+        if i<3 then liste_manche[i] := liste_cartes[i];
+        if i=high(liste_cartes) then atout := une_carte;
         end;
     joueurs_list := load_players(joueurs_list); (* initialisation des joueurs *)
     set_deck(liste_cartes);
@@ -41,7 +43,7 @@ begin
 
     while true do begin (* Boucle principale *)
         afficher_background(image); (* chargement du fond *)
-        afficher_atout(une_carte);
+        afficher_atout(atout);
         afficher_joueurs(joueurs_list); (* chargement des joueurs *)
         afficher_cartes; (* chargement des cartes *)
         afficher_texte(text,convert_couleur(blue));
@@ -51,16 +53,21 @@ begin
 
         gFlip();
 
-        while (sdl_update = 1) do (* si la fenêtre se met à jour (mouvement de la souris) *)
+
+        while (sdl_update = 1) do begin (* si la fenêtre se met à jour (mouvement de la souris) *)
             if (sdl_do_quit) then (* Clic sur la croix pour fermer *)
                 exit;
+                une_carte := on_click;
+                if une_carte.valeur<>-1 then writeln('carte cliquée: ',une_carte.valeur);
+            end;
 
         sleep(10);
 
         t += 1;
-        (* if t>5 then begin
+         if t>50 then begin
             set_joueur(joueurs_list[round(random*4)]);
             t := 0;
-            end; *)
+            end;
+
     end;
 end.
