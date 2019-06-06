7054b0b52601949c4a79e2c67948110e3169c90a
Z_runner <arthur.blaise.23@gmail.com>
2019-06-06 16:44:50 +0200

    Update classes.pas

--- classes.pas
+++ classes.pas
@@ -14,37 +14,36 @@ Type config = Record // configuration de la partie
     min_age:integer;
     max_age:integer;
     default_window_size:integer;
-    end;
+end;
 
 Type carte = Record // L'une des 52 cartes du jeu
     couleur:string;
     valeur:integer;
+    gcouleur:gColor;
+    texte_petit:gImage;
+    texte_grand:gImage;
+    x,y,w,h:real;
 end;
+Type cartesArray=array of carte;
 
 Type joueur=Record // L'un des joueurs
     cartes:array of carte;
     pseudo:string;
     age:integer;
     couleur:byte;
-    end;
-Type joueurs=array of joueur;
+    gcouleur:gColor;
+    x,y,r:integer;
+    pseudo_txt:gImage;
+end;
+Type joueursArray=array of joueur;
 
 
 Type deck = array[0..51] of carte; // Liste de toutes les cartes du jeu, triées
 
-type joueur_graph=Record // uniquement les infos utiles
-    pseudo:string;
-    couleur:gColor;
-    x,y:integer;
-    pseudo_txt:gImage;
-end;
-Type joueurs_graph=array of joueur_graph;
 
-type carte_graph=Record
-    couleur:gColor;
-    texte:gImage;
-end;
 
+type text_graph=gImage; // texte lisible par la lib graphique
+type color_graph=gColor;
 type background=gImage;
 
 
