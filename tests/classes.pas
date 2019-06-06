53801cb4e781d4d61c45996476349d81dc561709
Z_runner <arthur.blaise.23@gmail.com>
2019-06-06 15:22:49 +0200

    Update classes.pas

--- tests/classes.pas
+++ tests/classes.pas
@@ -14,7 +14,7 @@ Type config = Record // configuration de la partie
     min_age:integer;
     max_age:integer;
     default_window_size:integer;
-    end;
+end;
 
 Type carte = Record // L'une des 52 cartes du jeu
     couleur:string;
@@ -22,7 +22,9 @@ Type carte = Record // L'une des 52 cartes du jeu
     gcouleur:gColor;
     texte_petit:gImage;
     texte_grand:gImage;
+    x,y,w,h:real;
 end;
+Type cartesArray=array of carte;
 
 Type joueur=Record // L'un des joueurs
     cartes:array of carte;
@@ -30,10 +32,10 @@ Type joueur=Record // L'un des joueurs
     age:integer;
     couleur:byte;
     gcouleur:gColor;
-    x,y:integer;
+    x,y,r:integer;
     pseudo_txt:gImage;
-    end;
-Type joueurs=array of joueur;
+end;
+Type joueursArray=array of joueur;
 
 
 Type deck = array[0..51] of carte; // Liste de toutes les cartes du jeu, triées
