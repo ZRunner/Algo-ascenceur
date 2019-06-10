869a36748bbc53a5914d0f379aae71679a8e2998
MelyMelo8 <50695757+MelyMelo8@users.noreply.github.com>
2019-06-10 14:16:42 +0200

    Update classes.pas

--- classes.pas
+++ classes.pas
@@ -7,44 +7,45 @@ interface
 uses gLib2D;
 
 Type config = Record // configuration de la partie
-    players:integer;
-    win_defaut:integer;
-    win:integer;
-    loose:integer;
-    min_age:integer;
-    max_age:integer;
-    default_window_size:integer;
+    players : integer;
+    win_defaut : integer;
+    win : integer;
+    loose : integer;
+    min_age : integer;
+    max_age : integer;
+    default_window_size : integer;
 end;
 
 Type carte = Record // L'une des 52 cartes du jeu
-    couleur:string;
-    valeur:integer;
-    gcouleur:gColor;
-    texte_petit:gImage;
-    texte_grand:gImage;
-    x,y,w,h:real;
+    couleur : string;
+    valeur : integer;
+    gcouleur : gColor;
+    texte_petit : gImage;
+    texte_grand : gImage;
+    x,y,w,h : real;
 end;
-Type cartesArray=array of carte;
+Type cartesArray = array of carte;
 
-Type joueur=Record // L'un des joueurs
-    cartes:array of carte;
-    pseudo:string;
-    age:integer;
-    couleur:byte;
-    gcouleur:gColor;
-    x,y,r:integer;
-    pseudo_txt:gImage;
+Type joueur = Record // L'un des joueurs
+    cartes : array of carte;
+    pseudo : string;
+    age : integer;
+    couleur : byte;
+    pari : integer;
+    point : integer;
+    PliManche : integer; //nombre de pli remporté par manche
+    gcouleur : gColor;
+    x,y,r : integer;
+    pseudo_txt : gImage;
 end;
-Type joueursArray=array of joueur;
 
-
+Type joueursArray = array of joueur;
 Type deck = array[0..51] of carte; // Liste de toutes les cartes du jeu, triées
+Type joueurs = array of joueur; // Liste de tous les joueurs du jeu
 
-
-
-type text_graph=gImage; // texte lisible par la lib graphique
-type color_graph=gColor;
-type background=gImage;
+type text_graph = gImage; // texte lisible par la lib graphique
+type color_graph = gColor;
+type background = gImage;
 
 
 
